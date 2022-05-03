7f3e80a062567eb5656866462575f0b97be88fe4
Borko Jandras <bjandras@flowmill.com>
2022-05-03 18:43:15 -0500

    Supporting upgraded AWS SDK library in the new build-env.

--- cmake/aws-sdk.cmake
+++ cmake/aws-sdk.cmake
@@ -1,24 +1,24 @@
 find_package(AWSSDK REQUIRED)
 set(AWS_SERVICES ec2 s3 core)
 AWSSDK_DETERMINE_LIBS_TO_LINK(AWS_SERVICES AWSSDK_LIBS)
+
+# Unfortunately AWSSDK_DETERMINE_LIBS_TO_LINK will list SSL and Crypto libraries
+# simply as "ssl" and "crypto", not using the full path with which the AWS SDK
+# was configured inside the build-env.
+list(TRANSFORM AWSSDK_LIBS REPLACE "^ssl$" OpenSSL::SSL)
+list(TRANSFORM AWSSDK_LIBS REPLACE "^crypto$" OpenSSL::Crypto)
+
 add_library(aws-sdk-cpp INTERFACE)
 target_link_libraries(
   aws-sdk-cpp
   INTERFACE
-    -L${AWSSDK_LIB_DIR}
     ${AWSSDK_LIBS}
     ${AWSSDK_LIBS}
-    curl-static
-    z
+    s2n
+    dl
 )
 target_include_directories(
   aws-sdk-cpp
   INTERFACE
     ${AWSSDK_INCLUDE_DIR}
-)
-target_link_libraries(
-  aws-sdk-cpp
-  INTERFACE
-    OpenSSL::Crypto
-    -ldl
 )
