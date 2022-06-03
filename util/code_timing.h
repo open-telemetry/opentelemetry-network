2e838c558a9b6835d9c53d54ef7f745a3fa65f9f
Dustin Bingham <dbingham@splunk.com>
2022-06-03 11:38:47 -0600

    fix parsing error for line, add a prefix for code time internal stats

--- util/code_timing.h
+++ util/code_timing.h
@@ -37,7 +37,8 @@ public:
 
   void set(u64 duration_ns);
 
-  void write_stats(std::stringstream &ss, u64 timestamp, std::string const &common_labels = "");
+  void write_stats(
+      std::stringstream &ss, u64 timestamp, std::string_view internal_stats_prefix, std::string const &common_labels = "");
 
   void print();
 
@@ -59,7 +60,8 @@ public:
   void unregister_code_timing(std::string const &name);
 
   // Writes all registered code timings out as internal stats.
-  void write_stats(std::stringstream &ss, u64 timestamp, std::string const &common_labels = "");
+  void write_stats(
+      std::stringstream &ss, u64 timestamp, std::string_view internal_stats_prefix, std::string const &common_labels = "");
 
   // Prints all registered code timings out via LOG::info().
   void print();
