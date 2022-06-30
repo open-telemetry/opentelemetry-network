6051c0d9e0ab806bd7e544c812ff7820b28b7f93
Jim Wilson <86026167+jimwsplk@users.noreply.github.com>
2022-06-29 20:07:21 -0400

    Fix get_request_json(). (#66)

--- otlp/otlp_util.h
+++ otlp/otlp_util.h
@@ -21,7 +21,9 @@
 template <typename T> std::string get_request_json(const T &request)
 {
   std::string request_json_str;
-  google::protobuf::util::MessageToJsonString(
-      request, &request_json_str, {.add_whitespace = true, .always_print_primitive_fields = true});
+  google::protobuf::util::JsonPrintOptions json_print_options;
+  json_print_options.add_whitespace = true;
+  json_print_options.always_print_primitive_fields = true;
+  google::protobuf::util::MessageToJsonString(request, &request_json_str, json_print_options);
   return request_json_str;
 }
