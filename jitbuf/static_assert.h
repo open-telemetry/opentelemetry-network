ece15231438b86ae148fba6595c479d2ff2d1479
Borko Jandras <bjandras@flowmill.com>
2022-12-12 12:44:40 -0600

    Removing dead jitbuf code.

--- jitbuf/static_assert.h
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright The OpenTelemetry Authors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#pragma once
-#define JB_STATIC_ASSERT(name, predicate) typedef char _jitbuf_static_assert_##name[2 * !!(predicate)-1];
