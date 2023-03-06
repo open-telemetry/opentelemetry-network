ff0fdabe1ef76c28e963aa25c671498f3fe37428
samiura <samiura@splunk.com>
2023-03-06 14:27:56 -0800

    addressing Borko's comments.

--- reducer/ingest/agent_span.cc
+++ reducer/ingest/agent_span.cc
@@ -535,11 +535,12 @@ void AgentSpan::public_to_private_ipv4(
   private_mapped_addrs_.insert(private_addr);
 
   auto &addr_map = global_private_to_public_address_map();
-  auto existing_public_addr = addr_map.get(private_addr);
 
-  if (existing_public_addr && existing_public_addr == public_addr) {
-    local_logger().rewriting_private_to_public_ip_mapping(
-        jb_blob(private_addr.str()), jb_blob(existing_public->str()), jb_blob(public_addr.str()));
+  if (auto existing_public = addr_map.get(private_addr); existing_public) {
+    if (*existing_public == public_addr) {
+      local_logger().rewriting_private_to_public_ip_mapping(
+          jb_blob(private_addr.str()), jb_blob(existing_public->str()), jb_blob(public_addr.str()));
+    }
   }
 
   addr_map.insert(private_addr, public_addr);
