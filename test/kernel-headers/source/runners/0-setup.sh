ee2c7706b0ea9107c882bc171beddf83037a8888
samiura <samiura@splunk.com>
2023-03-24 12:40:20 -0700

    fixing last failed run

--- test/kernel-headers/source/runners/0-setup.sh
+++ test/kernel-headers/source/runners/0-setup.sh
@@ -2,8 +2,11 @@
 # Copyright The OpenTelemetry Authors
 # SPDX-License-Identifier: Apache-2.0
 
+echo $1
+
 EBPF_NET_SRC_ROOT="${EBPF_NET_SRC_ROOT:-$(git rev-parse --show-toplevel)}"
 set -x
+
 
 vagrant destroy -f || true
 [ -e .vagrant ] && rm -rf .vagrant
@@ -16,35 +19,45 @@ then
     exit 1
 fi
 
+
 # don't do this until after vagrant up so the script can check the result and cleanup and exit if it fails
 source "${EBPF_NET_SRC_ROOT}/dev/script/bash-error-lib.sh"
 
-# Check if local docker registry is running
-result=$(docker ps | grep registry:latest | grep local-docker-registry) || true
-if [[ "${result}" == "" ]]
-then
-  # local registry isn't running, so start it
-  "${EBPF_NET_SRC_ROOT}/dev/docker-registry.sh"
-fi
+vagrant plugin install vagrant-sshfs 
 
-# Check that required docker images exist
-result1=$(docker images | grep ^kernel-collector) || true
-result2=$(docker images | grep ^reducer) || true
-if [[ "${result1}" == "" || "${result2}" == "" ]]
-then
-  echo "ERROR: required docker image(s) do not exist!"
-  exit 1
-fi
+vagrant plugin install vagrant-scp 
 
-# Push docker images to local docker registry
-docker tag kernel-collector localhost:5000/kernel-collector:latest
-docker push localhost:5000/kernel-collector
-docker tag reducer localhost:5000/reducer:latest
-docker push localhost:5000/reducer
+if [ $# -eq 0 ]
+  then
+    # Check if local docker registry is running
+   result=$(docker ps | grep registry:latest | grep local-docker-registry) || true
+   if [[ "${result}" == "" ]]
+     then
+     # local registry isn't running, so start it
+      "${EBPF_NET_SRC_ROOT}/dev/docker-registry.sh"
+   fi
+
+  # Check that required docker images exist
+    result1=$(docker images | grep ^kernel-collector) || true
+    result2=$(docker images | grep ^reducer) || true
+    if [[ "${result1}" == "" || "${result2}" == "" ]]
+    then
+      echo "ERROR: required docker image(s) do not exist!"
+      exit 1
+    fi
+    # Push docker images to local docker registry
+    docker tag kernel-collector localhost:5000/kernel-collector:latest
+    docker push localhost:5000/kernel-collector
+    docker tag reducer localhost:5000/reducer:latest
+    docker push localhost:5000/reducer
+elif [ $# -eq 1 ]
+then
+    echo "Running inside github runner, skipping docker push to local registry"
+fi
 
 vagrant up --no-provision
 
-# Confirm that the vagrant VM is running
+# Confirm tha the vagrant VM is running
 result=$(vagrant status | grep ^default | grep running) || true
 if [[ "${result}" == "" ]]
 then
