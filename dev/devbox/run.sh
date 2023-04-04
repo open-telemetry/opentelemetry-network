f622fd73d941164f24c0af3d4c1fb98761415e0e
Borko Jandras <bjandras@splunk.com>
2023-04-04 18:48:45 -0500

    Use EBPF_NET_OUT_DIR if defined. (#150)

--- dev/devbox/run.sh
+++ dev/devbox/run.sh
@@ -8,7 +8,7 @@ source "${EBPF_NET_SRC_ROOT}/dev/script/bash-error-lib.sh"
 # shellcheck source=/dev/null
 source "${EBPF_NET_SRC_ROOT}/dev/script/benv-lib.sh"
 
-export EBPF_NET_OUT_DIR="$(get_benv_build_dir)"
+export EBPF_NET_OUT_DIR="${EBPF_NET_OUT_DIR:-$(get_benv_build_dir)}"
 
 vagrant up
 vagrant ssh -- -R 5000:localhost:5000 -L 58080:localhost:8080 -L 59090:localhost:9090
