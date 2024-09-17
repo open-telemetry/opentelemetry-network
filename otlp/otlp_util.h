0a1453a2145364cc7e15ff9336159a1b67bfb26a
jakub-racek-swi <jakub.racek@solarwinds.com>
2024-09-17 13:28:49 +0000

    Handle MessageToJsonString return value
    Signed-off-by: jakub-racek-swi <jakub.racek@solarwinds.com>

--- otlp/otlp_util.h
+++ otlp/otlp_util.h
@@ -13,6 +13,10 @@ template <typename T> std::string get_request_json(const T &request)
   google::protobuf::util::JsonPrintOptions json_print_options;
   json_print_options.add_whitespace = true;
   json_print_options.always_print_primitive_fields = true;
-  google::protobuf::util::MessageToJsonString(request, &request_json_str, json_print_options);
+  google::protobuf::util::Status status = google::protobuf::util::MessageToJsonString(request, &request_json_str, json_print_options);
+  if (!status.ok()) {
+    std::cerr << "Failed to convert message to JSON: " << status.ToString() << std::endl;
+    return "";
+  }
   return request_json_str;
 }
