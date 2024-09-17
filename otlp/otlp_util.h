79ff0cd645d14d7a1ce2da756e6f5debd5a121af
jakub-racek-swi <jakub.racek@solarwinds.com>
2024-09-17 13:53:19 +0000

    Set correct MessageToString return value
    Signed-off-by: jakub-racek-swi <jakub.racek@solarwinds.com>

--- otlp/otlp_util.h
+++ otlp/otlp_util.h
@@ -13,7 +13,7 @@ template <typename T> std::string get_request_json(const T &request)
   google::protobuf::util::JsonPrintOptions json_print_options;
   json_print_options.add_whitespace = true;
   json_print_options.always_print_primitive_fields = true;
-  google::protobuf::util::Status status = google::protobuf::util::MessageToJsonString(request, &request_json_str, json_print_options);
+  auto status = google::protobuf::util::MessageToJsonString(request, &request_json_str, json_print_options);
   if (!status.ok()) {
     std::cerr << "Failed to convert message to JSON: " << status.ToString() << std::endl;
     return "";
