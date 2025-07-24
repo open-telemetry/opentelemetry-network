a53a5654f5434163d8b5069d7d61a181200c2039
Jonathan Perry <yonch@yonch.com>
2025-07-24 17:44:45 +0000

    remove OpenSSL version check (we use the distro package)

--- cmake/openssl.cmake
+++ cmake/openssl.cmake
@@ -4,6 +4,3 @@
 include_guard()
 
 find_package(OpenSSL REQUIRED)
-if (NOT OPENSSL_VERSION STREQUAL "1.1.1w")
-  message(FATAL_ERROR "OpenSSL must be a specific version (1.1.1w). Build container should already have that set up")
-endif()
