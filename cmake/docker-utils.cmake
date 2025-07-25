e494aa6e850661cf1456d13c2aca67e8c87b4815
Jonathan Perry <yonch@yonch.com>
2025-07-25 05:30:39 +0000

    use podman when tagging docker images ahead of registry push

--- cmake/docker-utils.cmake
+++ cmake/docker-utils.cmake
@@ -189,7 +189,7 @@ function(build_custom_docker_image IMAGE_NAME)
         TARGET
           "${IMAGE_NAME}-docker-registry"
         COMMAND
-          docker tag "${IMAGE_NAME}" "${IMAGE_NAME}:${IMAGE_TAG}"
+          podman tag "${IMAGE_NAME}" "${IMAGE_NAME}:${IMAGE_TAG}"
         COMMAND
           "${CMAKE_SOURCE_DIR}/dev/docker-registry-push.sh"
             "${IMAGE_NAME}" "${IMAGE_TAG}" --no-login "${ARG_DOCKER_REGISTRY}"
