002d325da13fdcf66ce8e5ddbab21443cd46a007
Jonathan Perry <yonch@yonch.com>
2025-07-26 16:21:13 +0000

    fix string_starts_with without strlen

--- collector/kernel/bpf_src/tcp-processor/bpf_memory.h
+++ collector/kernel/bpf_src/tcp-processor/bpf_memory.h
@@ -16,14 +16,12 @@
 inline static int string_starts_with(const char *s1, const size_t s1_len, const char *s2)
 {
 
-  const size_t s2_len = strlen(s2);
+  char localdata[16] = {};
+  const size_t s2_len = bpf_probe_read_kernel_str(localdata, sizeof(localdata), s2);
 
   if (s1_len < s2_len) {
     return 0;
   }
-
-  char localdata[16] = {};
-  bpf_probe_read(localdata, s2_len, s1);
 
 #pragma passthrough on
 #pragma unroll
