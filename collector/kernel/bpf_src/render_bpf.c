bf58766b85d48e5d3fb91770b3e12bf379eece57
Jonathan Perry <yonch@yonch.com>
2025-07-29 02:50:45 -0400

    add license to bpf code

--- collector/kernel/bpf_src/render_bpf.c
+++ collector/kernel/bpf_src/render_bpf.c
@@ -2719,3 +2719,5 @@ int on_ctnetlink_dump_tuples(struct pt_regs *ctx, struct sk_buff *skb, const str
 //
 
 #include "tcp-processor/bpf_tcp_processor.c"
+
+char _license[] SEC("license") = "Dual GPL/Apache-2.0";
