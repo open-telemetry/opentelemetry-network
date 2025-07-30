bdedc0391e9c26d89955a169b1c4fabad1a20920
Jonathan Perry <yonch@yonch.com>
2025-07-30 12:29:51 +0300

    output pid_tgid in bpf_trace_printk in END_SAVE_ARGS to appease the verifier

--- collector/kernel/bpf_src/tcp-processor/bpf_types.h
+++ collector/kernel/bpf_src/tcp-processor/bpf_types.h
@@ -73,9 +73,9 @@ typedef u64 TIMESTAMP;
     /* Check if key already exists to distinguish duplicate vs table full */                                                   \
     void *existing = bpf_map_lookup_elem(&SAVED_ARGS_TABLE(FUNC), &SAVED_ARGS_TABLE_KEY);                                      \
     if (existing) {                                                                                                            \
-      bpf_trace_printk(#FUNC ": duplicate arg insert. tgid=%ull\n", (u64)(_tgid & 0xFFFFFFFF));                                \
+      bpf_trace_printk(#FUNC ": duplicate arg insert. pid_tgid=%ull\n", _pid_tgid);                                            \
     } else {                                                                                                                   \
-      bpf_trace_printk(#FUNC ": args table is full, dropped insert. tgid=%ull\n", (u64)(_tgid & 0xFFFFFFFF));                  \
+      bpf_trace_printk(#FUNC ": args table is full, dropped insert. pid_tgid=%ull\n", _pid_tgid));                             \
     }                                                                                                                          \
   }                                                                                                                            \
   }
