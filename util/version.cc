3699f9e7032c3273248517c4279616c3703749a1
Jonathan Perry <yonch@yonch.com>
2025-10-20 20:41:21 +0000

    feat(build): refactor version info to cc file to speed up build

---
+++ util/version.cc
@@ -0,0 +1,11 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#include <util/version_config.h>
+#include <util/version.h>
+
+namespace versions {
+
+extern const VersionInfo release{EBPF_NET_MAJOR_VERSION, EBPF_NET_MINOR_VERSION, EBPF_NET_PATCH_VERSION};
+
+} // namespace versions
